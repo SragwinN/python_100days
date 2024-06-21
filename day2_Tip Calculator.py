264af5a48b046f86c885561fcce6449c911d9dea
SragwinN <sragwinn.22.11.2001@gmail.com>
2024-06-21 14:58:23 -0500

    Update day2_Tip Calculator.py

--- day2_Tip Calculator.py
+++ day2_Tip Calculator.py
@@ -5,7 +5,6 @@ Tip_as_percentage = Tip/100
 Bill_with_tip = Bill*Tip_as_percentage + Bill
 people = int(input("How many people to split the bill?"))
 Total_Bill = Bill_with_tip / people
-bill_per_person = round(Total_Bill,2)
 bill_per_person = "{:.2f}".format(Total_Bill)
 print(f"Each person should pay: ${bill_per_person}")
 
